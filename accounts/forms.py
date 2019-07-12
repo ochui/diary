4130b9318b203d78e7b87e04ac328597dfcf42f0
ochui princewill patrick <ochuiprincewill411@gmail.com>
2019-07-12 21:32:15 +0100

    bootstrap 4

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,23 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget = forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Title',
+                'autofocus': 'autofocus'
+            }
+        )
+
+        self.fields['text'].widget = forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder': "What's on your mind" ,
+            }
+        )
     class Meta:
         model = Post
         fields = ('title', 'text',)
