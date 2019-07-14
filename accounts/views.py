3b8d722dcea5a98768e2196db9ed7888f239f654
ochui princewill patrick <ochuiprincewill411@gmail.com>
2019-07-14 21:52:02 +0100

    added search

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.views.generic import CreateView, TemplateView, DeleteView, UpdateVie
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView
+from django.db.models import Q
 from .models import Post
 from .forms import PostForm, RegisterForm, LoginForm
 
@@ -14,14 +15,18 @@ class SignUp(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
+
 class PostListView(LoginRequiredMixin, ListView):
 
     model = Post
     template_name = "post/post_list.html"
 
     def get_queryset(self):
-        return Post.objects.all()
-    
+        query = self.request.GET.get('q')
+        return Post.objects.filter(
+            Q(title__icontains=query) | Q(text__icontains=query)
+        )
+
 
 class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
@@ -29,35 +34,44 @@ class PostDetailView(LoginRequiredMixin, DetailView):
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
-    
+
     model = Post
     form_class = PostForm
     template_name = "post/post_create.html"
 
     def form_valid(self, form):
-        
+
         self.object = form.save(commit=False)
         self.object.author = self.request.user
         self.object.save()
         return super().form_valid(form)
 
+
 class CustomLoginView(LoginView):
-    
+
     form_class = LoginForm
 
-class DashboardView(ListView):
+
+class DashboardView(ListView, LoginRequiredMixin):
 
     model = Post
     template_name = "dashboard/dashboard.html"
 
     def get_queryset(self):
-        return Post.objects.all()
+        query = self.request.GET.get('q')
+        if query:
+            return Post.objects.filter(
+                Q(title__icontains=query) | Q(text__icontains=query)
+            )
+        else:
+            return Post.objects.all()
 
 
 class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = "post/post_create.html"
+
 
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
